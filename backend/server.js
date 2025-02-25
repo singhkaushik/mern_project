1cfb7c1f96a98e28d7fa5dd6edab4c6cecbc226c
singhkaushik <singhkaushik28@gmail.com>
2025-02-25 23:42:50 +0530

    something changes

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,8 @@ app.use(
   cors({
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    // allowedHeaders: ["Content-Type", "Authorization"],
+    // credentials: true,
   })
 );
 
